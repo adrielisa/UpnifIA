c6b6543130fcf27aef85a825e0c0fe6971556166
adrielisa <danielganer67@gmail.com>
2025-07-22 12:59:54 -0500

    Node fetch added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const https = require('https');
 const app = express();
 const port = 3000;
+const fetch = require('node-fetch');
 
 // Este es tu token real de Upnify
 const tkSesion = 'P07RTgwMkQ4RUMtN0I5RC00QUMyLTlFQzMtQ0MwRDY4MzVDMTE5';
